e5be119193903b57915980999f7f8be07e02d0b9
Chaitanya Shah <shahcg91@gmail.com>
2018-10-07 03:30:12 -0500

    Added env variables file

--- models/connection.py
+++ models/connection.py
@@ -1,17 +1,14 @@
 import pymysql
+from envs import DB_HOST, DB_DATABASE, DB_PASSWORD, DB_USER
 
 
 class Connection(object):
-    HOST = ""  # change before deploying
-    USER = ""
-    PASSWORD = ""
-    DATABASE = ""
 
     def __init__(self):
-        self.connection = pymysql.connect(user=self.USER,
-                                          password=self.PASSWORD,
-                                          host=self.HOST,
-                                          database=self.DATABASE,
+        self.connection = pymysql.connect(user=DB_USER,
+                                          password=DB_PASSWORD,
+                                          host=DB_HOST,
+                                          database=DB_DATABASE,
                                           cursorclass=pymysql.cursors.DictCursor)
         self.cursor = None
 
