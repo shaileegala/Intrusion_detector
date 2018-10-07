8a979caeddc36d586ec8e8004998474dd789e4f1
Chaitanya Shah <shahcg91@gmail.com>
2018-10-06 20:17:53 -0500

    Added pseudo models

--- models/models.py
+++ models/models.py
@@ -1,8 +1,27 @@
 
 # Validate username password
 class User(object):
-    pass
 
+    def __init__(self, id, username, fname, lname):
+        self.id = id
+        self.username = username
+        self.fname = fname
+        self.lname = lname
+
+    @classmethod
+    def fetch_by_username(cls, username):
+        query = ""
+
+        # Execute Query
+        # Fetch result
+
+        # If anything
+            # return User(<with all values put properly>)
+        # else:
+            # None
+
+
+# TODO
 
 # Get mean
 
